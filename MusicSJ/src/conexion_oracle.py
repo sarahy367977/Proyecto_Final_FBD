042fd3f7aeb3af854a01e9321c6463212dad178c
Jaz-Cruz <a367770@gmail.com>
2025-05-27 00:30:38 -0600

    Actualizacion de conexión y CRUD

--- MusicSJ/src/conexion_oracle.py
+++ MusicSJ/src/conexion_oracle.py
@@ -4,7 +4,7 @@ import datetime
 def conexion(user, password, dsn):
     return oracledb.connect(user=user, password=password, dsn=dsn)
 
-
+pip install flask
 def query_one(cursor, query):
     cursor.execute(query)
     row =  cursor.fetchone()
@@ -27,6 +27,7 @@ def insert_usuario(cursor, nombre, email, contrasena):
         return {'success': False, 'error': 'El correo ya está registrado.'}
     
     cursor.execute("insert into  usuario ( nombre, email, contrasena) values (:1, :2, :3)", [nombre, email, contrasena])
+    cursor.connection.commit()
     return {'success': True}
 
 def read_usuario(cursor, id_usuario):
@@ -43,7 +44,7 @@ def update_usuario(cursor, id_usuario, nuevo_nombre, nuevo_email, nueva_contrase
 
     if cursor.rowcount == 0:
         return {'success': False, 'error': 'Usuario no encontrado.'}
-    
+    cursor.connection.commit()
     return {'success': True}
 
 def delete_usuario(cursor, id_usuario):
@@ -51,11 +52,13 @@ def delete_usuario(cursor, id_usuario):
     if cursor.rowcount == 0:
         return {'success': False, 'message': 'No se encontró el usuario.'}
     else:
+        cursor.connection.commit()
         return {'success': True, 'message': 'Usuario eliminado correctamente.'}
 
 # CRUD Artista
 def insert_artista(cursor, nombre, pais, genero_principal):
     cursor.execute("insert into artista (nombre, pais, genero_principal) values (:1, :2, :3)", [nombre, pais, genero_principal])
+    cursor.connection.commit()
     cursor.execute("select seq_artista.currval from dual")
     id_generado = cursor.fetchone()[0]
     return {'success': True, 'id_artista': id_generado}
@@ -68,12 +71,14 @@ def delete_artista(cursor, id_artista):
     cursor.execute("delete from artista where id_artista = :1", [id_artista])
     if cursor.rowcount == 0:
         return {'success': False, 'error': 'Artista no encontrado.'}
+    cursor.connection.commit()
     return {'success': True}
 
 def update_artista(cursor, id_artista, nuevo_nombre, nuevo_genero_principal):
     cursor.execute("update artista set nombre = :1, genero_principal = :2 where id_artista = :3", [nuevo_nombre, nuevo_genero_principal, id_artista])
     if cursor.rowcount == 0:
         return {'success': False, 'error': 'Artista no encontrado.'}
+    cursor.connection.commit()
     return {'success': True}
 
 # CRUD genero
@@ -83,6 +88,7 @@ def insert_genero(cursor, nombre):
         return {'success': False, 'error': 'El género ya existe.'}
     
     cursor.execute("insert into genero (nombre) VALUES (:1)", [nombre])
+    cursor.connection.commit()
     return {'success': True, 'message': 'Género insertado correctamente.'}
 
 def read_genero(cursor, id_genero):
@@ -97,13 +103,14 @@ def update_genero(cursor, id_genero, nuevo_nombre):
     cursor.execute("update genero set nombre = :1 where id_genero = :2", [nuevo_nombre, id_genero])
     if cursor.rowcount == 0:
         return {'success': False, 'error': 'No se encontró el género.'}
-    
+    cursor.connection.commit()
     return {'success': True, 'message': 'El género actualizado correctamente.'}
 
 def delete_genero(cursor, id_genero):
     cursor.execute("delete from genero where id_genero = :1", [id_genero])
     if cursor.rowcount == 0:
         return {'success': False, 'error': 'El género no encontrado.'}
+    cursor.connection.commit()
     return {'success': True, 'message': 'El género eliminado correctamente.'}
 
 # cancion
@@ -114,6 +121,7 @@ def insert_cancion(cursor, titulo, id_album, id_genero, duracion):
         cursor.execute("select count(*) from cancion where titulo = :1 and id_album = :2", [titulo, id_album])
         
     cursor.execute("insert into cancion (titulo, id_album, id_genero, duracion) values (:1, :2, :3, :4)", [titulo, id_album, id_genero, duracion])
+    cursor.connection.commit()
     return {'success': True, 'message': 'Canción insertada correctamente.'}
 
 def read_cancion(cursor, id_cancion):
@@ -124,17 +132,20 @@ def update_cancion(cursor, id_cancion, nuevo_titulo, nueva_duracion):
     cursor.execute("update cancion set titulo = :1, duracion = :2 where id_cancion = :3", [nuevo_titulo, nueva_duracion, id_cancion])
     if cursor.rowcount == 0:
         return {'success': False, 'error': 'No se encontró la canción.'}
+    cursor.connection.commit()
     return {'success': True, 'message': 'Canción actualizada.'}
 
 def delete_cancion(cursor, id_cancion):
     cursor.execute("delete from cancion where id_cancion = :1", [id_cancion])
     if cursor.rowcount == 0:
         return {'success': False, 'error': 'Canción no encontrada.'}
+    cursor.connection.commit()
     return {'success': True, 'message': 'Canción eliminada.'}
 
 # CRUD playlist
 def insert_playlist(cursor, nombre, id_usuario):
     cursor.execute("insert into playlist (nombre, id_usuario) values (:1, :2)", [nombre, id_usuario])
+    cursor.connection.commit()
 
 def read_playlist(cursor, id_playlist):
     cursor.execute("select nombre, fecha_creacion, id_usuario from playlist where id_playlist = :1", [id_playlist])
@@ -142,9 +153,11 @@ def read_playlist(cursor, id_playlist):
 
 def update_playlist(cursor, id_playlist, nuevo_nombre):
     cursor.execute("update playlist set nombre = :1 where id_playlist = :2", [nuevo_nombre, id_playlist])
+    cursor.connection.commit()
 
 def delete_playlist(cursor, id_playlist):
     cursor.execute("delete from playlist where id_playlist = :1", [id_playlist])
+    cursor.connection.commit()
     return {'success': True, 'message': 'Playlist eliminada'}
 
 #Playlista cancion
@@ -154,6 +167,7 @@ def insert_playlist_cancion(cursor, id_playlist, id_cancion):
         return "La canción ya está en la playlist"
     try:
         cursor.execute("insert into playlist_cancion (id_playlist, id_cancion) values (:1, :2)", [id_playlist, id_cancion])
+        cursor.connection.commit()
         return "Canción insertada en la playlist"
     except Exception as e:
         return f"Error al insertar canción: {e}"
@@ -162,6 +176,7 @@ def delete_playlist_cancion(cursor, id_playlist, id_cancion):
     try:
         cursor.execute( "delete from playlist_cancion where id_playlist = :1 and id_cancion = :2", [id_playlist, id_cancion] )
         if cursor.rowcount > 0:
+            cursor.connection.commit()  
             return "Canción eliminada de la playlist"
         else:
             return "No se encontró la canción en la playlist"
@@ -171,6 +186,7 @@ def delete_playlist_cancion(cursor, id_playlist, id_cancion):
 # CRUD reproduccion
 def insert_reproduccion(cursor, id_usuario, id_cancion):
     cursor.execute("insert into reproduccion (id_usuario, id_cancion) values (:1, :2)", [id_usuario, id_cancion])
+    cursor.connection.commit()  
     return {'success': True, 'message': 'Reproducción insertada'}
 
 def read_all_reproducciones(cursor):
@@ -180,6 +196,7 @@ def read_all_reproducciones(cursor):
 def delete_reproduccion(cursor, id_reproduccion):
     cursor.execute("delete from reproduccion where id_reproduccion = :1", [id_reproduccion])
     if cursor.rowcount > 0:
+        cursor.connection.commit()
         return {'success': True, 'message': 'Reproducción eliminada'}
     else:
         return {'success': False, 'message': 'No se encontró la reproducción'}
@@ -187,6 +204,7 @@ def delete_reproduccion(cursor, id_reproduccion):
 #CRUD album
 def insert_album(cursor, id_artista, titulo, anio_lanzamiento):
     cursor.execute("insert into album (id_artista, titulo, anio_lanzamiento) values (:1, :2, :3)", [id_artista, titulo, anio_lanzamiento])
+    cursor.connection.commit()
     return {'success': True, 'message': 'Álbum insertado'}
 
 
@@ -200,11 +218,13 @@ def read_album(cursor, id_album):
 
 def update_album(cursor, id_artista, titulo, anio_lanzamiento, id_album):
     cursor.execute("update album set id_artista = :1, titulo = :2, anio_lanzamiento = :3 where id_album = :4", [id_artista, titulo, anio_lanzamiento, id_album])
+    cursor.connection.commit()
     return {'success': True, 'message': 'Álbum actualizado'}
 
 def delete_album(cursor, id_album):
     cursor.execute("delete from album where id_album = :1", [id_album])
     if cursor.rowcount > 0:
+        cursor.connection.commit()
         return {'success': True, 'message': 'Álbum eliminado'}
     else:
         return {'success': False, 'message': 'Álbum no encontrado'}
@@ -226,6 +246,7 @@ def insert_valoracion(cursor, puntuacion, comentario, id_cancion, id_usuario):
             id_valoracion = get_valoracion_id(cursor, id_usuario, id_cancion)
             if id_valoracion:
                 cursor.execute("update valoracion set puntuacion = :1, comentario = :2 where id_valoracion = :3", [puntuacion, comentario, id_valoracion])
+                cursor.connection.commit()
             else:
                 raise
 def read_valoracion(cursor, id_valoracion):
@@ -234,135 +255,5 @@ def read_valoracion(cursor, id_valoracion):
 
 def delete_valoracion(cursor, id_valoracion):
     cursor.execute("delete from valoracion where id_valoracion = :1", [id_valoracion])
-
-
-if __name__ == "__main__":
-    cnx = conexion("proyecto", "sj", "localhost/xepdb1")
-    cursor = cnx.cursor()
-
-    try:
-        # Consultas
-        query_one(cursor, "select * from usuario WHERE id_usuario = 1")
-        query_all(cursor, "select * from artista")
-
-        # prueba usuario
-        insert_usuario(cursor, "JuanaPerez", "jn@example.com", "12378647")
-        cnx.commit()
-
-
-        update_usuario(cursor, 1008, "Juanita", "jana@example.com", "83571121")
-        cnx.commit()
-
-
-        delete_usuario(cursor, 1029)
-        cnx.commit()
-
-        #prueba artista
-        insert_artista(cursor, "Artista_prueba", "México", "Pop")
-        cnx.commit()
-
-        update_result = update_artista(cursor, 1000, "Artista M", "Pop")
-        print("Resultado actualización artista:", update_result)
-        cnx.commit()
-
-        delete_result = delete_artista(cursor, 1031)
-        print("Resultado eliminación artista:", delete_result)
-        cnx.commit()
-        
-        #Prueba genero
-        resultado_insert = insert_genero(cursor, "Jazz")
-        print("Insertar género:", resultado_insert)
-        cnx.commit()
-
-        resultado_update = update_genero(cursor, 1, "Reggae")
-        print("Actualizar género:", resultado_update)
-        cnx.commit()
-
-        resultado_delete = delete_genero(cursor, 5)
-        print("Eliminar género:", resultado_delete)
-        cnx.commit()
-
-
-        #Canciones
-        resultado_insert = insert_cancion(cursor, "Mi Canción Test", 1009, 3 , 375)
-        print("Insertar canción:", resultado_insert)
-        cnx.commit()
-
-        resultado_update = update_cancion(cursor, 1007, "Canción Actualizada Test", 220)
-        print("Actualizar canción:", resultado_update)
-        cnx.commit()
-
-        resultado_delete = delete_cancion(cursor, 1034)
-        print("Eliminar canción:", resultado_delete)
-        cnx.commit()
-
-        #playlist
-        insert_playlist(cursor, "Mi Playlistt", 1027)
-        print("Insertar playlist: OK")
-        cnx.commit()
-
-        update_playlist(cursor,1007, "Nombre Playlist Actualizado2")
-        print("Actualizar playlist: OK")
-        cnx.commit()
-
-        delete_playlist(cursor, 1020)
-        print("Eliminar playlist: OK")
-        cnx.commit()
-
-        #playlist_cancion
-        resultado_insert = insert_playlist_cancion(cursor, 1021, 1026)
-        print("Insertar canción en playlist:", resultado_insert)
-        cnx.commit()
-
-        resultado_delete = delete_playlist_cancion(cursor, 1007, 1024)
-        print("Eliminar canción de playlist:", resultado_delete)
-        cnx.commit()
-
-        # Reproduccion
-        resultado_insert = insert_reproduccion(cursor, 1008, 1018)
-        print("Insertar reproducción:", resultado_insert)
-        cnx.commit()
-        
-        resultado_delete = delete_reproduccion(cursor, 1007)
-        print("Eliminar reproducción:", resultado_delete)
-        cnx.commit()
-
-        # Álbum
-        resultado_insert = insert_album(cursor, 1013, "Mi Álbum Test", datetime.date(2023, 5, 25))
-        print("Insertar álbum:", resultado_insert)
-        cnx.commit()
-
-        resultado_read = read_album(cursor, 1006)
-        print("Leer álbum", resultado_read)
-        cnx.commit()
-
-        album_actualizado= update_album(cursor, 1008, "Álbum modificado", datetime.date(2025, 1, 1), 1010)
-        print("album actualizado", album_actualizado)
-        cnx.commit()
-
-        album_eliminado = delete_album(cursor, 1003)
-        print("Album eliminado", album_eliminado)
-        cnx.commit()
-
-        #Valoracion
-        resultado_insert = insert_valoracion(cursor, 4, "Muy buena", 1013, 1009)
-        print("Insertar valoración:", resultado_insert)
-        cnx.commit()
-
-        resultado_leer = read_valoracion(cursor, 1006)
-        print("Leer valoración:", resultado_leer)
-
-        resultado_delete = delete_valoracion(cursor, 1004)
-        print("Eliminar valoración:", resultado_delete)
-        cnx.commit()
-
-
-        query_all(cursor, "select * from artista")
-        query_all(cursor, "select * from usuario")
-        query_all(cursor, "select * from genero")
-    except Exception as e:
-        print("Error durante la ejecucion :", e)
-        cnx.rollback()
-    finally:
-        cursor.close()
-        cnx.close()
+    if cursor.rowcount > 0:
+        cursor.connection.commit()
